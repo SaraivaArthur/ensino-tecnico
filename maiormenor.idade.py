9bd6de7d16defd9773ed067d6b92300a9af270f8
Arthur Saraiva <142699700+SaraivaArthur@users.noreply.github.com>
2025-04-09 13:41:36 -0300

    Create maiormenor.idade.py

---
+++ maiormenor.idade.py
@@ -0,0 +1,7 @@
+idade_minima = 18
+idade_desejada = int(input("Digite a sua idade: "))
+
+if idade_desejada > idade_minima:
+    print("Você é maior de idade")
+else:
+    print("Você é menor de idade")
